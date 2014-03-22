c5c7038737c655e50a432e2eb5dad64257958cf8
SoftwareSpot Apps <softwarespot@users.noreply.github.com>
2014-03-22 12:42:30 +0200

    Added: An efficient example of a while loop when counting from 0 to 20.

---
+++ while-loop-efficent/while-loop-efficent/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace while_loop_efficent
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            // Print all numbers between 0 and 20.
+
+            // Old way with 3 lines ( minus Console.WriteLine() and brackets ({}). )....
+            sbyte counter = 0; // Create a counter from 0.
+            while (counter <= 20) // Check if the counter is less than or equal to 20.
+            {
+                Console.Write(counter + ((counter != 20) ? "," : ".")); // Formatting commas and period only.
+                counter += 1; // Increase the counter by 1.
+            }
+
+            Console.WriteLine(""); // Write a new line.
+
+            // New way with 2 lines ( minus Console.WriteLine() and brackets ({}). )....
+            sbyte counter2 = -1; // Create a counter from -1.
+            while (++counter2 <= 20) // Increase the counter by 1 and check if the counter is less than or equal to 20.
+                Console.Write(counter2 + ((counter2 != 20) ? "," : ".")); // Formatting commas and period only.
+
+            Console.WriteLine(""); // Write a new line.
+
+            Console.WriteLine("\nBonus Round:"); // Bonus round.
+
+            // Third way with 2 lines ( minus Console.WriteLine() and brackets ({}). )....
+            sbyte counter3 = -1; // Create a counter from -1.
+            while ((counter3 += 1) <= 20) // Increase the counter by 1 and check if the counter is less than or equal to 20.
+                Console.Write(counter3 + ((counter3 != 20) ? "," : ".")); // Formatting commas and period only.
+
+            Console.WriteLine(""); // Write a new line.
+
+            Console.WriteLine("Please enter any key to continue...");
+            Console.ReadKey(true);
+        }
+    }
+}
