87ab5d4ba54b70b33a941bb0680687eb7f64bb38
SoftwareSpot Apps <softwarespot@users.noreply.github.com>
2014-03-22 12:53:09 +0200

    Added: An example of ++var and var++ (pre-increment and post-increment).

---
+++ pre-increment-and-post-increment/pre-increment-and-post-increment/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace pre_increment_and_post_increment
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            int value = 0;
+            value++; // Increase by 1.
+            Console.WriteLine("1. " + value); // (value = 1)
+            Console.WriteLine("2. " + ++value); // Increase by 1 and then return the value. (value = 2)
+            Console.WriteLine("3. " + value++); // Return the value and then increase by 1. (value = 2)
+            Console.WriteLine("3. " + ++value); // Increase by 1 and then return the value. (value = 4)
+            Console.WriteLine("4. " + value); // (value = 4)
+
+            Console.WriteLine("Please enter any key to continue...");
+            Console.ReadKey(true);
+        }
+    }
+}
