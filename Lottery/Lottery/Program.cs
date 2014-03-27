9407fb002cbb43703035772abfdc2feaa835f608
SoftwareSpot Apps <softwarespot@users.noreply.github.com>
2014-03-27 15:40:36 +0200

    Changed: Comments.

--- Lottery/Lottery/Program.cs
+++ Lottery/Lottery/Program.cs
@@ -13,7 +13,7 @@ namespace Lottery
          * if they had no relevance and were only informing the user. The numbers had to be between
          * 1 and 49, contain no duplicates and be sorted upon presentation to the user.
          *
-         * I won with just 5 lines! (Or 4 if you don't include the last Console.WriteLine() line.)
+         * I won with just 2 lines! (Or 1 if you don't include the last Console.WriteLine() line.)
          */
 
         public static void Main()
@@ -35,7 +35,7 @@ namespace Lottery
                 // Split into a string array with the comma character being the delimiter and replace non-digits to zero.
                 // Parse each element as an integer and sort the elements in sequential order.
                 // Creating a random integer array between 1 and 49: http://stackoverflow.com/questions/14118844/how-does-enumerable-orderby-use-keyselector with help from Joni for their idea with Enumerable.Range() and Take() method.
-                int[] userArray = Array.ConvertAll(Regex.Replace(Regex.Replace(Regex.Replace(Console.ReadLine(), @"(\d+)\D*", @"$1,"), @"^,|,$", ""), @"[^\d,]", @"0").Split(','), int.Parse).OrderBy(element => element).ToArray(), computerArray = Enumerable.Range(1, 49).OrderBy(element => Guid.NewGuid()).Take(7).OrderBy(element => element).ToArray();  // computerArray = { 0, 0, 0, 0, 0, 0, 0 }; // Integer arrays to compare later using SequenceEqual().
+                int[] userArray = Array.ConvertAll(Regex.Replace(Regex.Replace(Regex.Replace(Console.ReadLine(), @"(\d+)\D*", @"$1,"), @"^,|,$", ""), @"[^\d,]", @"0").Split(','), int.Parse).OrderBy(element => element).ToArray(), computerArray = Enumerable.Range(1, 49).OrderBy(element => Guid.NewGuid()).Take(7).OrderBy(element => element).ToArray();  // Integer arrays to compare later using SequenceEqual().
 
                 // Previous approach to generating a random number between 1 and 49.
                 // while ((computerArray[i[0]] = new Random().Next(1, 49)) == 0 || Array.FindAll(computerArray, element => element == computerArray[i[0]]).Length != 1 || computerArray[i[0]] == 0 || ++i[0] != computerArray.Length) { } // Check it's not already in the computer's array or equal to 0. Loop until it doesn't exist.
@@ -43,8 +43,10 @@ namespace Lottery
                 // OrderBy() and SequenceEqual() concept: http://stackoverflow.com/questions/50098/comparing-two-collections-for-equality-irrespective-of-the-order-of-items-in-the.
                 // Find similar matches between both arrays: http://www.dotnetperls.com/intersect.
 
-                // If the sort array does not have 7 elements OR the value is less than 1 OR the value is greater than 49 OR duplicate value exists, the FAIL, otherwise check if both int arrays are equal and display the matches found using intersect. // RegExp v2: (\d+),\1(?=,|$)
-                // Changed matching values less than 1 or greater than 49 to a regular expression http://regex101.com/r/lC9jX8. Old approach: Array.FindAll(userArray, element => element < 1 || element > 49).Length != 0
+                // If the sort array doesn't have 7 elements OR there is a value less than 1 OR there is a value greater than 49 OR duplicate values exist, then FAIL. Otherwise check if both integer arrays are equal is the jackpot otherwise display the matches found using Intersect() of alternative winnings.
+                // Changed matching values less than 1 or greater than 49 to a regular expression.  Old approach: Array.FindAll(userArray, element => element < 1 || element > 49).Length != 0
+                // Alternative regular expression for checking duplicates: (\d+),\1(?=,|$).
+                // Alternative regular expression for checking the bounds: http://regex101.com/r/lC9jX8.
                 Console.WriteLine((userArray.Length != 7 || Regex.IsMatch(string.Join(",", userArray), @"\b(0|(?:(?<=-)|[56789])\d+|\d{3,})\b") || Regex.IsMatch(string.Join(",", userArray), @"(\d+),\b\1\b")) ? "The values passed were incorrectly formatted and/or not between 1 and 49. Please try again.\n\n" : "User Lottery = " + string.Join(",", userArray) + "\nComputer Lottery = " + string.Join(",", computerArray) + "\n\nThe outcome was " + ((userArray.SequenceEqual(computerArray)) ? "you won! Congratulations.\n\n" : "you lost the jackpot!" + ((userArray.Intersect(computerArray).ToArray().Count() > 0) ? " But you had " + ((userArray.Intersect(computerArray).ToArray().Count() == 1) ? userArray.Intersect(computerArray).ToArray().Count() + " match " : userArray.Intersect(computerArray).ToArray().Count() + " matches ") + ((userArray.Intersect(computerArray).ToArray().Count() < 4) ? "and no win this time." : (userArray.Intersect(computerArray).ToArray().Count() == 4) ? "and won 400e." : (userArray.Intersect(computerArray).ToArray().Count() == 5) ? "and won 2300e." : (userArray.Intersect(computerArray).ToArray().Count() == 6) ? "and won 22000e." : "") : " Please try again.\n\n")));
             }
         }
