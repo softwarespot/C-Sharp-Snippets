31b23e52ffddba35e9a5e84e1a8e8b4277f4369f
SoftwareSpot Apps <softwarespot@users.noreply.github.com>
2014-03-28 02:48:20 +0200

    Added: E-mail verification using a regular expression.

--- Obfuscate-Mailto-Code/Obfuscate-Mailto-Code/Program.cs
+++ Obfuscate-Mailto-Code/Obfuscate-Mailto-Code/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 using System.Windows.Forms;
 
 namespace Obfuscate_Mailto_Code
@@ -17,34 +18,41 @@ namespace Obfuscate_Mailto_Code
              */
             Console.Write("Enter a valid e-mail address: ");
             string email = Console.ReadLine(), emailnew = string.Empty;
-            for (int i = 0; i < email.Length; i++)
-            {
-                emailnew += @"&#" + (int)email[i] + ";"; // Cast from char to an integer value.
-            }
-
-            string[] split = emailnew.Split(new String[] { "&#64;" }, StringSplitOptions.None);
-            if (split.Length != 2)
+            if (!Regex.IsMatch(email, @"^[A-Z0-9!#$%&'*+\-\/=?\^_`{|}~]+@[A-Z0-9.-]+\.[A-Z]{2,4}$", RegexOptions.IgnoreCase)) // Specification: http://en.wikipedia.org/wiki/E-mail_address
             {
                 Console.WriteLine("\nAn error occurred. Please check you entered a correct e-mail address.");
             }
             else
             {
-                string output = @"<script language=""Javascript"" type=""text/javascript"">" + Environment.NewLine;
-                output += "<!--" + Environment.NewLine;
-                output += "document.write('<a href=\"\"mai');" + Environment.NewLine;
-                output += "document.write('lto');" + Environment.NewLine;
-                output += "document.write(':" + split[0] + "');" + Environment.NewLine;
-                output += "document.write('@');" + Environment.NewLine;
-                output += "document.write('" + split[1] + "\"\">');" + Environment.NewLine;
-                output += "document.write('" + split[0] + "');" + Environment.NewLine;
-                output += "document.write('@');" + Environment.NewLine;
-                output += "document.write('" + split[1] + "<\\/a>');" + Environment.NewLine;
-                output += "// -->" + Environment.NewLine;
-                output += "</script><noscript>" + split[0] + " at " + Environment.NewLine;
-                output += split[1].Replace("&#46;", " dot ") + @"</noscript>";
+                for (int i = 0; i < email.Length; i++)
+                {
+                    emailnew += @"&#" + (int)email[i] + ";"; // Cast from char to an integer value.
+                }
 
-                Clipboard.SetText(output, TextDataFormat.Text);
-                Console.Write("\nThe following ouput has been placed on the clipboard.\n\n" + output + "\n");
+                string[] split = emailnew.Split(new String[] { "&#64;" }, StringSplitOptions.None);
+                if (split.Length != 2)
+                {
+                    Console.WriteLine("\nAn error occurred. Please check you entered a correct e-mail address.");
+                }
+                else
+                {
+                    string output = @"<script language=""Javascript"" type=""text/javascript"">" + Environment.NewLine;
+                    output += "<!--" + Environment.NewLine;
+                    output += "document.write('<a href=\"\"mai');" + Environment.NewLine;
+                    output += "document.write('lto');" + Environment.NewLine;
+                    output += "document.write(':" + split[0] + "');" + Environment.NewLine;
+                    output += "document.write('@');" + Environment.NewLine;
+                    output += "document.write('" + split[1] + "\"\">');" + Environment.NewLine;
+                    output += "document.write('" + split[0] + "');" + Environment.NewLine;
+                    output += "document.write('@');" + Environment.NewLine;
+                    output += "document.write('" + split[1] + "<\\/a>');" + Environment.NewLine;
+                    output += "// -->" + Environment.NewLine;
+                    output += "</script><noscript>" + split[0] + " at " + Environment.NewLine;
+                    output += split[1].Replace("&#46;", " dot ") + @"</noscript>";
+
+                    Clipboard.SetText(output, TextDataFormat.Text);
+                    Console.Write("\nThe following ouput has been placed on the clipboard.\n\n" + output + "\n");
+                }
             }
 
             Console.Write("\nPress any key to continue . . . ");
