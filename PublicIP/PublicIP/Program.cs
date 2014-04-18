6a15f208228ab3538c7e8a3e5c1c774d9f20fdf7
SoftwareSpot Apps <softwarespot@users.noreply.github.com>
2014-04-18 18:30:36 +0300

    Added: Get public IP class.

---
+++ PublicIP/PublicIP/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace PublicIP
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            PublicIP publicIP = new PublicIP();
+            Console.WriteLine("Public IP: {0}", publicIP.Get());
+            Console.WriteLine("IP discovery interval: {0}", publicIP.Interval);
+            Console.WriteLine("Public IP (ToString): {0}", publicIP.ToString());
+
+            Console.WriteLine(); // Empty line.
+
+            Console.WriteLine("Press any key to continue . . .");
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ PublicIP/PublicIP/PublicIP.cs
@@ -0,0 +1,69 @@
+﻿using System.Diagnostics;
+using System.Net;
+using System.Text.RegularExpressions;
+
+namespace PublicIP
+{
+    internal class PublicIP // Idea about DownloadString(): http://www.codeproject.com/Tips/452024/Getting-the-External-IP-Address
+    {
+        private string publicIP;
+        private Stopwatch timer;
+
+        /// <summary>
+        /// Retrieves the public IP address of the local network.
+        /// </summary>
+        public PublicIP() // Constructor.
+        {
+            Interval = 300000; // Property for how many milliseconds between each check. This is 5 minutes.
+            publicIP = null; // Assign as null.
+            timer = new Stopwatch(); // Create a new Stopwatch object.
+        }
+
+        ~PublicIP() // DeConstructor.
+        {
+            timer.Stop();
+            timer = null;
+        }
+
+        /// <summary>
+        /// Interval in milliseconds of how often the IP discovery site is queried.
+        /// </summary>
+        public int Interval
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Public IP address of the local network.
+        /// </summary>
+        /// <returns>Public IP address or null.</returns>
+        public string Get()
+        {
+            if (timer.IsRunning && timer.ElapsedMilliseconds < Interval && this.publicIP != null)
+            {
+                return this.publicIP;
+            }
+
+            timer.Reset(); // Reset the timer.
+            timer.Start(); // Start the timer from zero.
+            try
+            {
+                string publicIP = (new WebClient()).DownloadString("http://myexternalip.com/raw");
+                publicIP = (new Regex(@"((?:\d{1,3}\.){3}\d{1,3})")).Matches(publicIP)[0].ToString();
+                this.publicIP = publicIP;
+            }
+            catch { }
+            return this.publicIP;
+        }
+
+        /// <summary>
+        /// Public IP address of the local network.
+        /// </summary>
+        /// <returns>Public IP address of the local network.</returns>
+        public override string ToString()
+        {
+            return this.publicIP;
+        }
+    }
+}
