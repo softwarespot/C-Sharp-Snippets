d30fb762c737c29e7c380d96fd0e05cc177f6b75
SoftwareSpot Apps <softwarespot@users.noreply.github.com>
2014-04-18 13:11:27 +0300

    Added: PathCompactPathEx method.

---
+++ PathCompactPathEx/PathCompactPathEx/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace PathCompactPathEx
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine(PathCompactPathEx(@"C:\This_is_some_long_file_path\with_a_couple_of_folders\and_a\filename.txt", 20));
+            Console.ReadKey(true);
+        }
+
+        private static string PathCompactPathEx(string filePath, int length)
+        {
+            if (!string.IsNullOrEmpty(filePath) && (filePath.Length - Path.GetFileName(filePath).Length) > length) // Shorten the path but still retaining the file name length.
+            {
+                filePath = Regex.Replace(filePath, @"^(.{" + length + @"}).+?([^\\]+)$", @"$1...\$2");
+            }
+            return filePath;
+        }
+    }
+}
