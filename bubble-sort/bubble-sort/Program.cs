f9ac6199f4ead6105ac6097c2f4e37091ea606f7
SoftwareSpot Apps <softwarespot@users.noreply.github.com>
2014-05-08 08:31:28 +0300

    Changed: Example to be a class and fixed bug with bubble sort algorithm.

--- bubble-sort/bubble-sort/Program.cs
+++ bubble-sort/bubble-sort/Program.cs
@@ -2,40 +2,85 @@
 
 namespace bubble_sort
 {
-    internal class Program
+    internal class BubbleSort
     {
-        private static void Main()
+        public static void Quick(int[] array)
         {
-            int[] userChoice = { 6, 5, 1, 10, 15, 4, 3, 20, 21, 22 }; // An array of integers.
-
             // This is a simple bubble sort for an array of integers.
-            for (int i = 0; i < userChoice.Length; i++)
+            for (int i = 0; i < array.Length; i++)
             {
-                bool isSorted = true; // Set a boolean value to true on whether or not changes occurred.
-                for (int j = i + 1; j < userChoice.Length; j++)
+                for (int j = 0; j < array.Length - 1 - i; j++)
                 {
-                    if (userChoice[i] > userChoice[j]) // Check each index against one another to see if the next value is greater than the other.
+                    int nextElement = j + 1;
+                    if (array[j] > array[nextElement]) // Check each index against one another to see if the next value is greater than the other.
                     {
-                        int temp = userChoice[i]; // The temp variable is declared in "loop scope" as this is the only location it is used.
-                        userChoice[i] = userChoice[j]; // Swap the contents of the array elements.
-                        userChoice[j] = temp;
-                        isSorted = false; // If a change was made then set isSorted to false. The array still needs to be checked.
+                        int temp = array[j]; // The temp variable is declared in "loop scope" as this is the only location it is used.
+                        array[j] = array[nextElement]; // Swap the contents of the array elements.
+                        array[nextElement] = temp;
                     }
                 }
-                if (isSorted) // If no changes occurred then break the loop early. In this example it forces the loop iterations from 55 down to 49. (EXPERT ONLY!)
-                    break;
             }
+        }
 
-            Console.Write("The sorted values are: ");
-            for (int i = 0; i < userChoice.Length; i++)
+        public static void Standard(int[] array)
+        {
+            // This is a simple bubble sort for an array of integers.
+            for (int i = 0; i < array.Length; i++)
             {
-                Console.Write(userChoice[i] + ((i != userChoice.Length - 1) ? ", " : ".")); // Formatting commas and period only.
+                for (int j = 0; j < array.Length - 1; j++)
+                {
+                    int nextElement = j + 1;
+                    if (array[j] > array[nextElement]) // Check each index against one another to see if the next value is greater than the other.
+                    {
+                        int temp = array[j]; // The temp variable is declared in "loop scope" as this is the only location it is used.
+                        array[j] = array[nextElement]; // Swap the contents of the array elements.
+                        array[nextElement] = temp;
+                    }
+                }
             }
+        }
+    }
+
+    internal class Program
+    {
+        private static void CreateArray(int[] array)
+        {
+            Random random = new Random(Environment.TickCount);
+            for (int i = 0, lastElement = array.Length - 1; i <= lastElement; i++)
+            {
+                array[i] = random.Next(1, 1000);
+            }
+        }
+
+        private static void Main()
+        {
+            int[] a1 = new int[5], a2 = new int[a1.Length]; // An array of integers.
+            CreateArray(a1); // Create random numbers in the integer array.
+            Array.Copy(a1, a2, a2.Length); // Copy the first array.
+
+            PrintArray(a1); // Print the unsorted values.
+            BubbleSort.Standard(a1); // Sort the array using the standard bubble sort.
+            PrintArray(a1); // Print the sorted values.
 
             Console.WriteLine(); // Empty line.
 
+            PrintArray(a2); // Print the unsorted values.
+            BubbleSort.Quick(a2); // Sort the array using the quick bubble sort.
+            PrintArray(a2); // Print the sorted values.
+
+            Console.WriteLine(); // Empty line.
             Console.WriteLine("Press any key to continue . . .");
             Console.ReadKey(true);
+        }
+        private static void PrintArray(int[] array)
+        {
+            Console.Write("The values are: ");
+            int lastElement = array.Length - 1;
+            for (int i = 0; i <= lastElement; i++)
+            {
+                Console.Write(array[i] + ((i != lastElement) ? ", " : ".")); // Formatting commas and period only.
+            }
+            Console.WriteLine(); // Empty line.
         }
     }
 }
