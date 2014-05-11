1687bfd258d743e17bfd46b7b5c2816750957b69
SoftwareSpot Apps <softwarespot@users.noreply.github.com>
2014-05-11 22:59:01 +0300

    Changed: Find the square root of the maximum value.

--- IsPrimeNumber/IsPrimeNumber/Program.cs
+++ IsPrimeNumber/IsPrimeNumber/Program.cs
@@ -4,6 +4,26 @@ namespace IsPrimeNumber
 {
     internal class Program
     {
+        private static bool IsPrimeNumber(int number, out int? divisibleBy) // divisibleBy is a nullable datatype.
+        {
+            divisibleBy = null; // Set the out parameter as null, for those numbers which are prime values.
+            if (number <= 1)
+            {
+                divisibleBy = 1;
+                return false; // Return false if the value is less than or equal to one.
+            }
+
+            for (int i = 2; i <= Math.Sqrt(number); i++)
+            {
+                if (number % i == 0) // If there was no remainder means it's not a prime number.
+                {
+                    divisibleBy = i; // Set the out param as the divisible by value.
+                    return false;
+                }
+            }
+            return true; // This means it's a prime number.
+        }
+
         private static void Main()
         {
             int userChoice;
@@ -28,26 +48,6 @@ namespace IsPrimeNumber
 
             Console.WriteLine("Press any key to continue . . .");
             Console.ReadKey(true);
-        }
-
-        private static bool IsPrimeNumber(int number, out int? divisibleBy) // divisibleBy is a nullable datatype.
-        {
-            divisibleBy = null; // Set the out parameter as null, for those numbers which are prime values.
-            if (number <= 1)
-            {
-                divisibleBy = 1;
-                return false; // Return false if the value is less than or equal to one.
-            }
-
-            for (int i = 2; i <= number / 2; i++) // Divide the number by 2 as it's the highest value that will "go in to" the number e.g. if the value was 20 then 11 or above doesn't fit into 20.
-            {
-                if (number % i == 0) // If there was no remainder means it's not a prime number.
-                {
-                    divisibleBy = i; // Set the out param as the divisible by value.
-                    return false;
-                }
-            }
-            return true; // This means it's a prime number.
         }
     }
 }
