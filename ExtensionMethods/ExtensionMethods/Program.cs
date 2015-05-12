a3f3ba4d2d4e6a35da7fc9d1cfc52d30765c4526
SoftwareSpot Apps <softwarespot@users.noreply.github.com>
2015-05-12 23:56:07 +0300

    Code refactoring and tidying up

--- ExtensionMethods/ExtensionMethods/Program.cs
+++ ExtensionMethods/ExtensionMethods/Program.cs
@@ -1,257 +1,239 @@
 ﻿using CustomMethod;
 
 using System;
+using System.Linq;
 using System.Text.RegularExpressions;
 
 namespace CustomMethod
 {
-    public static class Extensions // Create a custom class for extension methods.
+    public static class ExtensionMethods // Create a custom class for extension methods.
     {
         /// <summary>
-        /// Checks whether a character is a digit from 0-9.
+        /// Checks whether a character is a digit from 0-9
         /// </summary>
-        /// <param name="c">A character value.</param>
+        /// <param name="value">A character value</param>
         /// <returns>True or False</returns>
-        public static bool IsDigit(this char c)
+        public static bool IsDigit(this char value)
         {
-            return (c >= '0' && c <= '9');
+            return value >= '0' && value <= '9';
         }
 
         /// <summary>
-        /// Checks whether a string is a digits from 0-9.
+        /// Checks whether a string is a digits from 0-9
         /// </summary>
-        /// <param name="i">A string value.</param>
+        /// <param name="value">A string value</param>
         /// <returns>True or False</returns>
-        public static bool IsDigit(this string i)
+        public static bool IsDigit(this string value)
         {
-            foreach (char value in i)
+            return value.All(@int => @int >= '0' && @int <= '9');
+        }
+
+        /// <summary>
+        /// Checks whether a double is an even value
+        /// </summary>
+        /// <param name="value">A double value</param>
+        /// <returns>True or False</returns>
+        public static bool IsEven(this double value)
+        {
+            int abs = (int)Math.Abs(Math.Round(value) % 2);
+            return abs == 0;
+        }
+
+        /// <summary>
+        /// Checks whether an integer is an even value
+        /// </summary>
+        /// <param name="value">An integer value</param>
+        /// <returns>True or False</returns>
+        public static bool IsEven(this int value)
+        {
+            return value % 2 == 0;
+        }
+
+        /// <summary>
+        /// Checks whether a character is a hexadecimal value from 0-9 and A-F
+        /// </summary>
+        /// <param name="value">A character value</param>
+        /// <returns>True or False</returns>
+        public static bool IsHex(this char value)
+        {
+            return value >= '0' && value <= '9' ||
+                value >= 'A' && value <= 'F' ||
+                value >= 'a' && value <= 'f';
+        }
+
+        /// <summary>
+        /// Checks whether a string is a hexadecimal value from 0-9 and A-F
+        /// </summary>
+        /// <param name="value">A string value</param>
+        /// <returns>True or False</returns>
+        public static bool IsHex(this string value)
+        {
+            return value.All(s => (s >= '0' && s <= '9') || (s >= 'A' && s <= 'F') || (s >= 'a' && s <= 'f'));
+        }
+
+        /// <summary>
+        /// Checks if a double is an odd value
+        /// </summary>
+        /// <param name="value">A double value</param>
+        /// <returns>True or False</returns>
+        public static bool IsOdd(this double value)
+        {
+            int abs = (int)Math.Abs(Math.Round(value) % 2);
+            return abs != 0;
+        }
+
+        /// <summary>
+        /// Checks if an integer is an odd value
+        /// </summary>
+        /// <param name="value">An integer value</param>
+        /// <returns>True or False</returns>
+        public static bool IsOdd(this int value)
+        {
+            return value % 2 != 0;
+        }
+
+        /// <summary>
+        /// Checks whether a string is a palindrome
+        /// </summary>
+        /// <param name="value">A string value</param>
+        /// <returns>True or False</returns>
+        public static bool IsPalindrome(this string value)
+        {
+            value = Regex.Replace(value, @"\W", "").ToLower(); // Remove all non-word characters and convert to lowercase
+            char[] array = value.ToCharArray();
+            Array.Reverse(array);
+            return new string(array) == value;
+        }
+
+        /// <summary>
+        /// Checks if a byte value is a prime number
+        /// </summary>
+        /// <param name="value">A byte value</param>
+        /// <returns>True or False</returns>
+        public static bool IsPrime(this byte value) // Idea from: http://montreal.pm.org/tech/neil_kandalgaonkar.shtml
+        {
+            return Regex.IsMatch(new string('1', value), @"^(1?|(11+?)\2+)$", RegexOptions.None);
+        }
+
+        /// <summary>
+        /// Checks if an integer value is a prime number
+        /// </summary>
+        /// <param name="value">An integer value</param>
+        /// <returns>True or False</returns>
+        public static bool IsPrime(this int value) // Idea from: http://montreal.pm.org/tech/neil_kandalgaonkar.shtml
+        {
+            return Regex.IsMatch(new string('1', value), @"^(1?|(11+?)\2+)$", RegexOptions.None);
+        }
+
+        /// <summary>
+        /// Checks if a byte value is true when non-zero (Like in most languages)
+        /// </summary>
+        /// <param name="value">A byte value</param>
+        /// <returns>True or False</returns>
+        public static bool IsTrue(this byte value)
+        {
+            return value != 0;
+        }
+
+        /// <summary>
+        /// Checks if a double value is true when non-zero. (Like in most languages)
+        /// </summary>
+        /// <param name="value">A double value</param>
+        /// <returns>True or False</returns>
+        public static bool IsTrue(this double value)
+        {
+            int abs = (int)Math.Abs(value);
+            return abs < 0 || abs > 0;
+        }
+
+        /// <summary>
+        /// Checks if an integer value is true when non-zero. (Like in most languages)
+        /// </summary>
+        /// <param name="value">An integer value</param>
+        /// <returns>True or False</returns>
+        public static bool IsTrue(this int value)
+        {
+            return value != 0;
+        }
+
+        /// <summary>
+        /// Checks if a string is true when the value is not empty. (Like in most languages)
+        /// </summary>
+        /// <param name="value">A string value</param>
+        /// <returns>True or False</returns>
+        public static bool IsTrue(this string value)
+        {
+            return !String.IsNullOrEmpty(value);
+        }
+
+        /// <summary>
+        /// Repeat a character a certain number of times
+        /// </summary>
+        /// <param name="value">A character value</param>
+        /// <param name="count">Number of times to repeat the character</param>
+        /// <returns>Repeated character</returns>
+        public static string Repeat(this char value, int count)
+        {
+            return (count < 0) ? value.ToString() : new string(value, count);
+        }
+
+        /// <summary>
+        /// Repeat a string a certain number of times
+        /// </summary>
+        /// <param name="value">A string value</param>
+        /// <param name="count">Number of times to repeat the string</param>
+        /// <returns>Repeated string</returns>
+        public static string Repeat(this string value, int count) // Idea taken from AutoIt _StringRepeat()
+        {
+            if (count < 0 || value.Length < 1) // Check the string length is greater than 1 and the repeat count is valid. Zero is considered valid
+                return value;
+
+            return string.Join(value, new string[count + 1]); // http://rosettacode.org/wiki/Repeat_a_string#C.23
+        }
+
+        /// <summary>
+        /// Reverse a string
+        /// </summary>
+        /// <param name="value">A string value</param>
+        /// <returns>Reversed string</returns>
+        public static string Reverse(this string value)
+        {
+            char[] array = value.ToCharArray();
+            int i = 0, j = array.Length - 1;
+            while (i < j) // This version is a slightly optimised version than Array.Reverse()
             {
-                if (value < '0' || value > '9')
-                    return false;
+                char temp = array[i];
+                array[i++] = array[j];
+                array[j--] = temp;
             }
-            return true;
-        }
-
-        /// <summary>
-        /// Checks whether a double is an even value.
-        /// </summary>
-        /// <param name="d">A double value.</param>
-        /// <returns>True or False</returns>
-        public static bool IsEven(this double d)
-        {
-            return Math.Round(d) % 2 == 0;
-        }
-
-        /// <summary>
-        /// Checks whether an integer is an even value.
-        /// </summary>
-        /// <param name="i">An integer value.</param>
-        /// <returns>True or False</returns>
-        public static bool IsEven(this int i)
-        {
-            return i % 2 == 0;
-        }
-
-        /// <summary>
-        /// Checks whether a character is a hexadecimal value from 0-9 and A-F.
-        /// </summary>
-        /// <param name="c">A character value.</param>
-        /// <returns>True or False</returns>
-        public static bool IsHex(this char c)
-        {
-            return (c >= '0' && c <= '9' ||
-                c >= 'A' && c <= 'F' ||
-                c >= 'a' && c <= 'f');
-        }
-
-        /// <summary>
-        /// Checks whether a string is a hexadecimal value from 0-9 and A-F.
-        /// </summary>
-        /// <param name="s">A string value.</param>
-        /// <returns>True or False</returns>
-        public static bool IsHex(this string s)
-        {
-            foreach (char value in s)
-            {
-                if (value < '0' || value > '9' &&
-                    value < 'A' || value > 'F' &&
-                    value < 'a' || value > 'f')
-                    return false;
-            }
-            return true;
-        }
-
-        /// <summary>
-        /// Checks if a double is an odd value.
-        /// </summary>
-        /// <param name="d">A double value.</param>
-        /// <returns>True or False</returns>
-        public static bool IsOdd(this double d)
-        {
-            return Math.Round(d) % 2 != 0;
-        }
-
-        /// <summary>
-        /// Checks if an integer is an odd value.
-        /// </summary>
-        /// <param name="i">An integer value.</param>
-        /// <returns>True or False</returns>
-        public static bool IsOdd(this int i)
-        {
-            return i % 2 != 0;
-        }
-
-        /// <summary>
-        /// Checks whether a string is a palindrome.
-        /// </summary>
-        /// <param name="s">A string value.</param>
-        /// <returns>True or False</returns>
-        public static bool IsPalindrome(this string s)
-        {
-            s = Regex.Replace(s, @"\W", "").ToLower(); // Remove all non-word characters and convert to lowercase.
-            char[] c = s.ToCharArray();
-            Array.Reverse(c);
-            return new string(c) == s;
-        }
-
-        /// <summary>
-        /// Checks if a byte value is a prime number.
-        /// </summary>
-        /// <param name="b">A byte value.</param>
-        /// <returns>True or False</returns>
-        public static bool IsPrime(this byte b) // Idea from: http://montreal.pm.org/tech/neil_kandalgaonkar.shtml
-        {
-            return Regex.IsMatch(new string('1', b), @"^(1?|(11+?)\2+)$", RegexOptions.None);
-        }
-
-        /// <summary>
-        /// Checks if an integer value is a prime number.
-        /// </summary>
-        /// <param name="i">An integer value.</param>
-        /// <returns>True or False</returns>
-        public static bool IsPrime(this int i) // Idea from: http://montreal.pm.org/tech/neil_kandalgaonkar.shtml
-        {
-            return Regex.IsMatch(new string('1', i), @"^(1?|(11+?)\2+)$", RegexOptions.None);
-        }
-
-        /// <summary>
-        /// Checks if a byte value is true when non-zero. (Like in most languages.)
-        /// </summary>
-        /// <param name="b">A byte value.</param>
-        /// <returns>True or False</returns>
-        public static bool IsTrue(this byte b)
-        {
-            return b != 0;
-        }
-
-        /// <summary>
-        /// Checks if a double value is true when non-zero. (Like in most languages.)
-        /// </summary>
-        /// <param name="d">A double value.</param>
-        /// <returns>True or False</returns>
-        public static bool IsTrue(this double d)
-        {
-            return d != 0;
-        }
-
-        /// <summary>
-        /// Checks if an integer value is true when non-zero. (Like in most languages.)
-        /// </summary>
-        /// <param name="i">An integer value.</param>
-        /// <returns>True or False</returns>
-        public static bool IsTrue(this int i)
-        {
-            return i != 0;
-        }
-
-        /// <summary>
-        /// Checks if a string is true when the value is not empty. (Like in most languages.)
-        /// </summary>
-        /// <param name="s">A string value.</param>
-        /// <returns>True or False</returns>
-        public static bool IsTrue(this string s)
-        {
-            return s.Length != 0;
-        }
-
-        /// <summary>
-        /// Repeat a character a certain number of times.
-        /// </summary>
-        /// <param name="c">A character value.</param>
-        /// <param name="count">Number of times to repeat the character.</param>
-        /// <returns>Repeated character.</returns>
-        public static string Repeat(this char c, int count)
-        {
-            return (count < 0) ? c.ToString() : new string(c, count);
-        }
-
-        /// <summary>
-        /// Repeat a string a certain number of times.
-        /// </summary>
-        /// <param name="s">A string value.</param>
-        /// <param name="count">Number of times to repeat the string.</param>
-        /// <returns>Repeated string.</returns>
-        public static string Repeat(this string s, int count) // Idea taken from AutoIt _StringRepeat()
-        {
-            if (count < 0 || s.Length < 1) // Check the string length is greater than 1 and the repeat count is valid. Zero is considered valid.
-                return s;
-
-            return string.Join(s, new string[count + 1]); // http://rosettacode.org/wiki/Repeat_a_string#C.23
-        }
-
-        /// <summary>
-        /// Reverse a string.
-        /// </summary>
-        /// <param name="s">A string value.</param>
-        /// <returns>Reversed string.</returns>
-        public static string Reverse(this string s)
-        {
-            char[] c = s.ToCharArray();
-            int i = 0, j = c.Length - 1;
-            while (i < j) // This version is a slightly optimised version than Array.Reverse().
-            {
-                char temp = c[i];
-                c[i++] = c[j];
-                c[j--] = temp;
-            }
-            return new string(c);
+            return new string(array);
 
             /*
-            char[] c = s.ToCharArray();
-            Array.Reverse(c);
-            return new string(c);
+             * char[] c = s.ToCharArray();
+             * Array.Reverse(c);
+             * return new string(c);
             */
         }
 
         /// <summary>
-        /// Convert an integer to a hexadecimal representation.
+        /// Convert an integer to a hexadecimal representation
         /// </summary>
-        /// <param name="i">An integer value.</param>
-        /// <param name="isLowerCase">Is the representation lowercase (true) or uppercase (false).</param>
-        /// <returns>Hexadecimal string.</returns>
-        public static string ToHexString(this int i, bool isLowerCase = true) // Convert an integer to a hex string representation.
+        /// <param name="value">An integer value</param>
+        /// <param name="isLowerCase">Is the representation lowercase (true) or uppercase (false)</param>
+        /// <returns>Hexadecimal string</returns>
+        public static string ToHexString(this int value, bool isLowerCase = true) // Convert an integer to a hex string representation
         {
-            return i.ToString((isLowerCase) ? "x" : "X");
+            return value.ToString((isLowerCase) ? "x" : "X");
         }
 
-        private static string ToLowerFast(this string s) // (Faster) convert a string to lowercase characters. Access the same way as .ToLower().
+        private static string ToLowerFast(this string value) // (Faster) convert a string to lowercase characters. Access the same way as .ToLower()
         {
-            string lowerString = string.Empty;
-            foreach (char value in s)
-            {
-                lowerString += (char)((value >= 'A' && value <= 'Z') ? (value + 32) : value); // Cast as a char.
-            }
-            return lowerString;
+            return value.Aggregate(String.Empty, (current, @char) => current + (char)((@char >= 'A' && @char <= 'Z') ? (@char + 32) : @char));
         }
 
-        private static string ToUpperFast(this string s) // (Faster) convert a string to uppercase characters. Access the same way as .ToUpper().
+        private static string ToUpperFast(this string value) // (Faster) convert a string to uppercase characters. Access the same way as .ToUpper()
         {
-            string upperString = string.Empty;
-            foreach (char value in s)
-            {
-                upperString += (char)((value >= 'a' && value <= 'z') ? (value - 32) : value); // Cast as a char.
-            }
-            return upperString;
+            return value.Aggregate(String.Empty, (current, @char) => current + (char)((@char >= 'a' && @char <= 'z') ? (@char - 32) : @char));
         }
     }
 }
